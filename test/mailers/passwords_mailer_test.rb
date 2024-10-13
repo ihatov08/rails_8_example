495e583ab245a87d5c1af2ec65ab414b1bd0dd87
Tomoya Yoshida <ihatov08@gmail.com>
2024-10-14 07:44:58 +0900

    rails g mailer passwords_mailer reset

--- test/mailers/passwords_mailer_test.rb
+++ test/mailers/passwords_mailer_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class PasswordMailerTest < ActionMailer::TestCase
+class PasswordsMailerTest < ActionMailer::TestCase
   test "reset" do
-    mail = PasswordMailer.reset
+    mail = PasswordsMailer.reset
     assert_equal "Reset", mail.subject
     assert_equal [ "to@example.org" ], mail.to
     assert_equal [ "from@example.com" ], mail.from
