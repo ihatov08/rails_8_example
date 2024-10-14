f4b2d0ef7edbfa0efaa05957febf343732ddee25
Tomoya Yoshida <ihatov08@gmail.com>
2024-10-14 09:36:46 +0900

    signup用のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
   resource :session
+  resource :registration, only: %i[ new create ]
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
