50a93676cea3d598092cf5b8f2ac6b14a1d769d1
Tomoya Yoshida <ihatov08@gmail.com>
2024-10-14 10:14:57 +0900

    signup機能実装

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,15 @@
 class RegistrationsController < ApplicationController
+  allow_unauthenticated_access
   def new
   end
 
   def create
+    user = User.new(params.permit(:email_address, :password))
+    if user.save
+      redirect_to new_session_path, notice: "Signed up successfully"
+    else
+      flash.now[:alert] = "Failed to sign up"
+      render :new
+    end
   end
 end
